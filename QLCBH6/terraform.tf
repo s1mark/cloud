abe3792c18e037e8849a111b309f0416ade4a2d8
kissolga0904 <kissolga0904@gmail.com>
2022-10-06 11:15:56 +0200

    terraform practice

--- QLCBH6/terraform.tf
+++ QLCBH6/terraform.tf
@@ -1,8 +1,5 @@
-resource "local_file" "foo"{
-    content = "foo!"
-    filename = ".\\test"
-}
-resource "local_file" "some_other_file" {
-    content = "value in the file"
-    filename = ".\\simple_file"
+resource "local_file" "textf_map" {
+  for_each = var.files
+  content = each.value
+  filename = each.key
 }
