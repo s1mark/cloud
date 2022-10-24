205925c52add8668b8d1f2e8c9bc8d03860c3895
Matuch Richárd <matuchrichard@citromail.hu>
2022-10-24 12:59:49 +0200

    Gcloud compute instance creation

--- Matuch Richard - I652B8/terraform_compute_admin/main.tf
+++ Matuch Richard - I652B8/terraform_compute_admin/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "ekke-cloud"
+  name         = "ekke-cloud-i652b8"
   machine_type = "f1-micro"
   zone         = "europe-central2-a"
 
