edaff1881ded3e824f478b1d67a78ca364628b76
Mark Simon <simark2357@gmail.com>
2022-11-03 10:24:07 +0100

    Remote buecket and iam module

--- _core/thursday/iam/main.tf
+++ _core/thursday/iam/main.tf
@@ -1,20 +1,6 @@
-resource "google_project_iam_binding" "project" {
-  project = "ekke-cloud"
-  role    = "roles/compute.instanceAdmin"
 
-  members = var.users
-}
-
-resource "google_project_iam_binding" "osLogin" {
-  project = "ekke-cloud"
-  role    = "roles/compute.osAdminLogin"
-
-  members = var.users
-}
-
-resource "google_project_iam_binding" "iap" {
-  project = "ekke-cloud"
-  role    = "roles/iap.tunnelResourceAccessor"
-
-  members = var.users
-}
+"ekke-cloud"
+var.users
+"roles/compute.instanceAdmin"
+"roles/compute.osAdminLogin"
+"roles/iap.tunnelResourceAccessor"
