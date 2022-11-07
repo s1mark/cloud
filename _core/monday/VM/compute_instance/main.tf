12499db4b69ec53aeeba88c6cce74d9063acd477
Mark Simon <simark2357@gmail.com>
2022-11-07 13:04:23 +0100

    Source fix and backend

--- _core/monday/VM/compute_instance/main.tf
+++ _core/monday/VM/compute_instance/main.tf
@@ -1,21 +1,15 @@
 module "instance_template" {
-  source          = "git::https://github.com/terraform-google-modules/terraform-google-vm/tree/master//modules/instance_template"
+  source          = "git::https://github.com/terraform-google-modules/terraform-google-vm//modules/instance_template?ref=v7.9.0"
   region          = var.region
   project_id      = var.project_id
   service_account = var.service_account
 }
 
 module "compute_instance" {
-  source              = "git::https://github.com/terraform-google-modules/terraform-google-vm/tree/master//modules/compute_instance"
+  source              = "git::https://github.com/terraform-google-modules/terraform-google-vm//modules/compute_instance?ref=v7.9.0"
   region              = var.region
   zone                = var.zone
   hostname            = "ekke-cloud"
   instance_template   = module.instance_template.self_link
   deletion_protection = false
-}
-
-module "instance_template" {
-  source                       = "git::https://github.com/terraform-google-modules/terraform-google-vm/tree/master//modules/instance_template"
-  project_id                   = var.project_id
-  service_account              = var.service_account
 }
