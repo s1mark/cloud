15ccb77f76ecddca6479bbe2892140bf8e61bf49
Matuch Richárd <matuchrichard@citromail.hu>
2022-11-07 12:51:17 +0100

    Final modifications

--- _core/monday/api/main.tf
+++ _core/monday/api/main.tf
@@ -4,15 +4,5 @@ module "project-services" {
   enable_apis                 = var.enable
   disable_services_on_destroy = true
 
-  activate_apis = [
-    "sqladmin.googleapis.com",
-    "bigquery-json.googleapis.com",
-  ]
-  activate_api_identities = [{
-    api = "healthcare.googleapis.com"
-    roles = [
-      "roles/healthcare.serviceAgent",
-      "roles/bigquery.jobUser",
-    ]
-  }]
+  activate_apis = var.activate_apis
 }
--- _core/monday/api/outputs.tf
+++ _core/monday/api/outputs.tf
@@ -1,9 +1,7 @@
 output "project_id" {
   value       = module.project-services.project_id
-  description = "The GCP project you want to enable APIs on"
 }
 
 output "enabled_apis" {
-  description = "Enabled APIs in the project"
   value       = [for api in module.project_services : api.service]
 }
