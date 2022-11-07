970da4d411bbc0bc2bff0dbbaf031f40225735a1
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-11-07 12:49:23 +0100

    terraform-google-vm_v2

--- _core/monday/VM/compute_instance/main.tf
+++ _core/monday/VM/compute_instance/main.tf
@@ -2,7 +2,6 @@ module "instance_template" {
   source          = "git::https://github.com/terraform-google-modules/terraform-google-vm/tree/master//modules/instance_template"
   region          = var.region
   project_id      = var.project_id
-  subnetwork      = var.subnetwork
   service_account = var.service_account
 }
 
@@ -10,14 +9,13 @@ module "compute_instance" {
   source              = "git::https://github.com/terraform-google-modules/terraform-google-vm/tree/master//modules/compute_instance"
   region              = var.region
   zone                = var.zone
-  subnetwork          = var.subnetwork
-  num_instances       = var.num_instances
-  hostname            = "instance-simple"
+  hostname            = "ekke-cloud"
   instance_template   = module.instance_template.self_link
   deletion_protection = false
+}
 
-  access_config = [{
-    nat_ip       = var.nat_ip
-    network_tier = var.network_tier
-  }, ]
+module "instance_template" {
+  source                       = "git::https://github.com/terraform-google-modules/terraform-google-vm/tree/master//modules/instance_template"
+  project_id                   = var.project_id
+  service_account              = var.service_account
 }
--- _core/monday/VM/instance_template/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "self_link" {
-  description = "Self-link to the instance template"
-  value       = module.instance_template.self_link
-}
-
-output "name" {
-  description = "Name of the instance templates"
-  value       = module.instance_template.name
-}
