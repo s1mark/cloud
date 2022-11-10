8dc1f605c5b5169175079c80b5e755dbdecebcaf
Vanessza02 <janosikvanessza@gmail.com>
2022-11-10 10:59:11 +0100

    2 params variables

--- ZH5ED7-LGP/modul/vm/main.tf
+++ ZH5ED7-LGP/modul/vm/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   machine_type = var.machine-type
   name         = var.vm-name
-  zone         = var.zone
+  zone         = "europe-central2-a"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
--- ZH5ED7-LGP/modul/vm/variables.tf
+++ ZH5ED7-LGP/modul/vm/variables.tf
@@ -2,10 +2,7 @@ variable "vm-name" {
   type = string
   default = "myVm-ZH5ED7"
 }
-variable "zone" {
-  type = string
-  default = "europe-central2-a"
-}
+
 variable "machine-type" {
   type = string
   default = "f1-micro"
