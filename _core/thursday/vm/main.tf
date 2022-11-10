41f2442f9de862a44c8c91c35e9b9ec24638104d
Vanessza02 <janosikvanessza@gmail.com>
2022-11-10 11:16:36 +0100

    asd

--- _core/thursday/vm/main.tf
+++ _core/thursday/vm/main.tf
@@ -18,9 +18,7 @@ resource "google_compute_instance" "default" {
 module "api" {
   source = "../api"
 }
-module "cl3zhy_vm" {
-  source = "../../../CL3ZHY/modul/vm"
-}
+
 module "myvm-zh5ed7" {
   source = "../../../ZH5ED7-LGP/modul/vm"
 }
