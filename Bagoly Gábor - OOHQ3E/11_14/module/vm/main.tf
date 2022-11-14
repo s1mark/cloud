1f03982627b232d8d155f6342be7e0739e5b58d8
OOHQ3E <gaboly15@gmail.com>
2022-11-14 12:20:59 +0100

    nov14 vm with module use

--- Bagoly Gábor - OOHQ3E/11_14/module/vm/main.tf
+++ Bagoly Gábor - OOHQ3E/11_14/module/vm/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "ekke-cloud"
+  name         = "ekke-cloud-oohq3e"
   machine_type = "f1-micro"
   zone         = "europe-central2-a"
 
@@ -11,8 +11,5 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-  }
-  module "vm-oohq3e" {
-  source = "../../../Bagoly Gábor - OOHQ3E/11_14/module/vm"
   }
 }
