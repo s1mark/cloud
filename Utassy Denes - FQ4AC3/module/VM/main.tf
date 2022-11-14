f4585d50e0537c9f3fab19a1d6bed5ffd4794661
Utassy Dénes <utassy.denis@gmail.com>
2022-11-14 12:18:13 +0100

    Added files to VM

---
+++ Utassy Denes - FQ4AC3/module/VM/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "default" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
---
+++ Utassy Denes - FQ4AC3/module/VM/variables.tf
@@ -0,0 +1,21 @@
+variable "name"{
+    description = "The name of the VM"
+    type = string
+    default = "ekke-cloud-FQ4AC3"
+}
+variable "machine_type"{
+    description = "The type of machine"
+    type = string
+    default = "f1-micro"
+}
+variable "zone" {
+    description = "Timezone of the VM"
+    type = string
+    default = "europe-central2-a"
+  
+}
+variable "image"{
+    description = "The image the VM uses"
+    type = string
+    default = "debian-cloud/debian-11"
+}
