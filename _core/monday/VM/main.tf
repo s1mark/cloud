8e2fb66ae5e6b897b438f33689cf8087cf371173
Matuch Richárd <matuchrichard@citromail.hu>
2022-11-14 12:20:16 +0100

    Fixing main.tf in _core

--- _core/monday/VM/main.tf
+++ _core/monday/VM/main.tf
@@ -1,4 +1,19 @@
-module "module-i652b8" {
-	source = "../../../Matuch Richard - I652B8/module/vm/"
-	}
+resource "google_compute_instance" "default" {
+  name         = "ekke-cloud"
+  machine_type = "f1-micro"
+  zone         = "europe-central2-a"
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+module "module-i652b8" {
+	source = "../../../Matuch Richard - I652B8/module/vm"
+}
