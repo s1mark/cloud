d979ae25292769353f9b6d631f9d117392aa8c04
D. Tóth Gyula Bátor <batordtoth@gmail.com>
2022-11-17 11:07:39 +0100

    massage

---
+++ EOMUBI/moduk/vm/main.tf
@@ -0,0 +1,20 @@
+resource "google_compute_instance" "default" {
+  name         = "ekke-cloud"
+  machine_type = "f1-micro"
+  zone         = "europe-central2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+  depends_on = [module.api]
+}
+
+module "EOMUBI_avi" {
+  source = "../../../_core/monday/VM"
+}
