40f206d5251541de1c6de2e77a4fd847576cbd22
Balázs Tolvaj <tolvibalazs@gmail.com>
2022-11-21 12:29:52 +0100

    Unnecessary parts removed

--- Tolvaj Balazs - DIZ39O/module/vm/provider.tf
+++ Tolvaj Balazs - DIZ39O/module/vm/provider.tf
@@ -2,10 +2,3 @@ provider "google" {
     project = var.project_name
     region  = var.provider_region
 }
-
-terraform {
-    backend "gcs" {
-        bucket  = "ekke-monday"
-        prefix  = "terraform/monday/vm"
-    }
-}
