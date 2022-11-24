e89d080d2d38f34080ad6748426d7174ea869ec6
Balázs Tolvaj <tolvibalazs@gmail.com>
2022-11-24 18:45:03 +0100

    Resources moved to main.tf
    #90

--- Tolvaj Balazs - DIZ39O/beadando/main.tf
+++ Tolvaj Balazs - DIZ39O/beadando/main.tf
@@ -21,6 +21,20 @@ resource "google_compute_instance" "default" {
   }
 }
 
+resource "google_compute_disk" "default" {
+  name  =  var.project_neptun_code
+  type  = "pd-standard"
+  zone  = var.provider_zone
+  image = var.vm_instance_image
+  size  = 10
+  physical_block_size_bytes = 4096
+}
+
+resource "google_service_account" "default" {
+  account_id   = var.project_neptun_code
+  display_name = var.project_neptun_code
+}
+
 terraform {
   backend "gcs" {
     bucket  = "diz39o"
--- Tolvaj Balazs - DIZ39O/beadando/provider.tf
+++ Tolvaj Balazs - DIZ39O/beadando/provider.tf
@@ -8,18 +8,3 @@ provider "google" {
 #    location = var.provider_location
 #    uniform_bucket_level_access = true   
 #}
-
-resource "google_compute_disk" "default" {
-  name  =  var.project_neptun_code
-  type  = "pd-standard"
-  zone  = var.provider_zone
-  image = var.vm_instance_image
-  size  = 10
-  physical_block_size_bytes = 4096
-}
-
-resource "google_service_account" "default" {
-  account_id   = var.project_neptun_code
-  display_name = var.project_neptun_code
-}
-
