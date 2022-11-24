b97f1578ce791ff86fb2cdca5df4fff5ab4584a0
Ádám Jakab <adam.jakab@lombiq.com>
2022-11-24 16:18:12 +0100

    Adding beadando files

---
+++ EENUGW/beadando/main.tf
@@ -0,0 +1,41 @@
+resource "google_compute_instance" "instance" {
+  name         = var.vm_instance_name
+  machine_type = var.vm_machine_type
+  zone         = var.project_zone
+
+  boot_disk {
+    source = google_compute_disk.disk.self_link
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+    email  = google_service_account.service_account.email
+    scopes = ["monitoring"]
+  }
+
+  metadata_startup_script = "echo ${var.file_content} >> ${var.file_name}"
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = var.neptun_code
+  display_name = "EENUGW"
+}
+
+resource "google_compute_disk" "disk" {
+  name                      = var.neptun_code
+  type                      = var.disk_type
+  zone                      = var.project_zone
+  image                     = var.disk_image
+  size                      = 10
+  physical_block_size_bytes = 4096
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "eenugw"
+    prefix  = "terraform/state"
+  }
+}
---
+++ EENUGW/beadando/outputs.tf
@@ -0,0 +1,36 @@
+data "google_project" "project" { 
+
+}
+
+data "google_compute_disk" "disk" { 
+  name = var.neptun_code
+  zone = var.project_zone
+}
+
+output "project_number" {
+  value = data.google_project.project.number
+}
+
+output "project_id" {
+  value = data.google_project.project.id
+}
+
+output "project_name" {
+  value = data.google_project.project.name
+}
+
+output "disk_size" {
+  value = data.google_compute_disk.disk.size
+}
+
+output "disk_image" {
+  value = data.google_compute_disk.disk.image
+}
+
+output "disk_type" {
+  value = data.google_compute_disk.disk.type
+}
+
+output "disk_self_link" {
+  value = data.google_compute_disk.disk.self_link
+}
