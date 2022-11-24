5a4bc1031956ea53718ca285d02a1f097723ec5a
gpita220 <galvacs11@gmail.com>
2022-11-24 11:18:36 +0100

    JHPIPM-Beadandó

---
+++ JHPIPM/Beadandó/main.tf
@@ -0,0 +1,6 @@
+module "y0dm28-module" {
+  source = "../module/vm"
+  service_account_display_name    = "jhpipm"
+	vm_name                         = "ekke-cloud-jhpipm"
+	disk_name                       = "jhpipm-disk"
+}
---
+++ JHPIPM/Beadandó/modul/vm/variables.tf
@@ -0,0 +1,16 @@
+variable "project"{
+    type =string
+    default="ekke-cloud"    
+}
+variable "zone" {
+  type=string
+  default="europe-central2-a"
+}
+variable "name"{
+    type = string
+    default = "ekke-cloud-jhpipm"
+}
+variable "machine_type" {
+  type=string
+  default="f1-micro"
+}
