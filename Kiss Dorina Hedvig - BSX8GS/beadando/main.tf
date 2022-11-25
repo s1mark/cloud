c9d240f4b0bb884090acf5b24a3092e81b1c5137
BSX8GS <k.dorina11@freemail.hu>
2022-11-25 02:33:34 +0100

    Beadandó fájlok
    main.tf, outputs.tf, provider.tf és variables.tf

---
+++ Kiss Dorina Hedvig - BSX8GS/beadando/main.tf
@@ -0,0 +1,44 @@
+resource "google_service_account" "default" {
+  account_id   = var.vm_name
+  display_name = var.sa_name
+  project_id = var.project
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.vm_name
+  machine_type = var.vm_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+
+    attached_disk {
+        source = google_compute_disk.default.id
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+metadata_startup_script = "echo done > ~/test.txt"
+
+resource "google_compute_disk" "default" {
+      name = "bsx8gs"
+      type = "pd-standard"
+      image = "debian-11-bullseye-v20220719"
+      zone = var.zone
+      size = 10
+      physical_block_size_bytes = 4096
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+data "google_compute_instance" "vm_data" {
+  name = google_compute_instance.default.name
+}
