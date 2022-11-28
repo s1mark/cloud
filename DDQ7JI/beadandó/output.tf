e8ffbc0c35afd82a2d74ad0674c1a4703724f04c
Vaplatin <Bpuszta@gmail.com>
2022-11-28 12:09:09 +0100

    update on output.tf

--- DDQ7JI/beadandó/output.tf
+++ DDQ7JI/beadandó/output.tf
@@ -1,11 +1,11 @@
 output "project_id" {
-  value = data.google_project.project.project.id
+  value = data.google_project.project.project_id
 }
 
 output "project_name" {
-  value = data.google_project.project_name
+  value = data.google_project.project.project_name
 }
 
 output "project_number" {
-  value = data.google_project.project_number
+  value = data.google_project.project.project_number
 }
