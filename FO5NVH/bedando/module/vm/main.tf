affcf9973951092844cb080dc715152911ea6575
Horváth Márton <horvathmarton02@gmail.com>
2022-11-30 23:43:24 +0100

    moved some variable in vm

--- FO5NVH/bedando/module/vm/main.tf
+++ FO5NVH/bedando/module/vm/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = var.vm_name
+  name         = var.neptun_kod
   machine_type = var.vm_mach_type
   zone         = var.vm_zone
 
@@ -15,7 +15,7 @@ resource "google_compute_instance" "default" {
 }
 
 resource "google_service_account" "default" {
-  account_id   = "fo5nvh"
+  account_id   = var.neptun_kod
 }
 
 service_account {
@@ -26,12 +26,12 @@ service_account {
 metadata_startup_script = "echo done > ~/task"
 
 resource "google_compute_disk" "attached_disk" {
-  name  = "fo5nvh"
-  type  = "pd-standard"
-  zone  = "europe-central2-a"
-  image = "debian-11-bullseye-v20220719"
-  size = "10"
-  physical_block_size_bytes = "4096"
+  name  = var.neptun_kod
+  type  = var.disk_type
+  zone  = var.disk_zone
+  image = var.disk_image
+  size = var.disk_size
+  physical_block_size_bytes = var.disk_block_size
 }
 
 attached_disk {
--- FO5NVH/bedando/module/vm/variables.tf
+++ FO5NVH/bedando/module/vm/variables.tf
@@ -1,8 +1,9 @@
-variable "vm_name" {
+variable "neptun_kod" {
   type = string
-  default = "fo5nvh-vm"
+  default = "fo5nvh"
 }
 
+//vm
 variable "vm_mach_type" {
   type = string
   default = "f1-micro"
@@ -23,3 +24,29 @@ variable "vm_network" {
   default = "default"
 }
 
+//disk
+
+variable "disk_type" {
+  type = string
+  default = "pd-standard"
+}
+
+variable "disk_zone" {
+  type = string
+  default = "europe-central2-a"
+}
+
+variable "disk_image" {
+  type = string
+  default = "debian-11-bullseye-v20220719"
+}
+
+variable "disk_size" {
+  type = string
+  default = "10"
+}
+
+variable "disk_block_size" {
+  type = string
+  default = "4096"
+}
