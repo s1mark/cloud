279cfd520cc21e7cd61d411b19a983617a159573
Horváth Márton <horvathmarton02@gmail.com>
2022-11-30 22:17:25 +0100

    Added bucket to provider.tf

--- FO5NVH/bedando/provider.tf
+++ FO5NVH/bedando/provider.tf
@@ -2,3 +2,10 @@ provider "google" {
   project     = "ekke-cloud"
   region      = "europe-central2"
 }
+
+terraform {
+ backend "gcs" {
+   bucket  = "fo5nvh"
+   prefix  = "terraform/fo5nvh"
+ }
+}
