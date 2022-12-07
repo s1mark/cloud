82408ad114e7ae4a3e71edb9a5cfd72ddb228ca4
unknown <imre123456.ing5@gmail.com>
2022-12-07 17:47:28 +0100

    CAZJQD

---
+++ CAZJQD/beadando/main.tf
@@ -0,0 +1,47 @@
+resource "google_service_account" "default" {
+  account_id   = "cazjqd-beadando"
+  display_name = var.sa_name
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.vm_name
+  machine_type = var.vm_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+
+  }
+
+  attached_disk {
+    source = google_compute_disk.default.id
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+    service_account {
+    email  = google_service_account.default.email
+    scopes = ["monitoring"]
+  }
+
+  metadata_startup_script = "echo done > ~/test.txt"
+}
+
+resource "google_compute_disk" "default" {
+      name = "cazjqd-attached-disk"
+      type = "pd-standard"
+      image = "debian-cloud/debian-11-bullseye-v20220719"
+      zone = var.zone
+      size = 10
+      physical_block_size_bytes = 4096
+
+}
+
+data "google_compute_instance" "vm_data" {
+  name = google_compute_instance.default.name
+  zone = google_compute_instance.default.zone
+}
