cdf10278e4ca599563183ad3f8f58a369904b54e
Mark Simon <simark2357@gmail.com>
2022-12-08 10:59:04 +0100

    typo fix

--- EOMUBI/Beadandó/main.tf
+++ EOMUBI/Beadandó/main.tf
@@ -1,5 +1,5 @@
 module "service_accounts" {
-    source = "terraform-google-modules/service-acounts/google"
+    source = "terraform-google-modules/service-accounts/google"
     names = ["eomubi"]
     zone = "europe-central2-a"
     version = "~> 3.0"
