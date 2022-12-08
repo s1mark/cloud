0f5ebbebdbba5cad7ad384acc6e3c3c185cc143f
Mark Simon <simark2357@gmail.com>
2022-12-08 11:01:02 +0100

    fixes

--- EOMUBI/Beadandó/main.tf
+++ EOMUBI/Beadandó/main.tf
@@ -1,7 +1,7 @@
 module "service_accounts" {
     source = "terraform-google-modules/service-accounts/google"
+    project_id = "ekke-cloud"
     names = ["eomubi"]
-    zone = "europe-central2-a"
     version = "~> 3.0"
     project_roles = [
         "ekke-cloud=>roles/viewer"
